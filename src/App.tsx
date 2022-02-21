a607ce5751a7a016370f44c954d3944afd91708b
lschnell8 <lschnell8@gmail.com>
2022-02-20 21:14:08 -0700

    Add typing to pass props and display components for home page

--- src/App.tsx
+++ src/App.tsx
@@ -4,21 +4,28 @@ import  { getData }  from './APICall';
 import Header from './Components/Header/Header'
 import RandomJokeContainer from './Components/RandomJokeContainer/RandomJokeContainer';
 
-class App extends Component {
-  state = {
 
+
+class App extends Component{
+  state = {
+    iconURL: '',
+    id: '',
+    value: ''
   }
 
   componentDidMount() {
     getData()
-    .then(data => data.value)
+      .then(data => {
+        this.setState({ iconURL: data.icon_url, id: data.id, value: data.value })
+        console.log(this.state)
+      })
   }
 
   render () {
     return (
       <main className="app">
         <Header />
-        <RandomJokeContainer />
+        <RandomJokeContainer chuckJoke={this.state.value} icon={this.state.iconURL} id={this.state.id}/>
       </main>
     )
   }
